111f8fe22b9f28240ca2fc709d2de3aedb57ff33
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-24 13:41:32 -0300

    removed unused web fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,19 +14,9 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="pt-BR">
+      <Html lang="en-US">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Public+Sans&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
